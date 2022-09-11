c9bd9b13165290c65689600d7244fc47ef65f33b
Joel <joelsetterberg@hotmail.com>
2022-09-11 13:27:06 +0200

    commit

--- src/components/SBLinkButtonV3.vue
+++ src/components/SBLinkButtonV3.vue
@@ -1,7 +1,6 @@
 <template lang="">
-  <p>SBLinkButtonV3</p>
-<div class="sb-link" :style="cssVars.value">
- <RouterLink v-if="buttonData" :to="buttonData.link || ''" :target="target">
+<div class="sb-link">
+ <RouterLink v-if="buttonData" :to="buttonData.link || ''">
    {{ buttonData.text }}
  </RouterLink>
 </div>
@@ -23,44 +22,45 @@ const props = defineProps<IProps>();
 </script>
 
 <style lang="scss">
-//Temporary colors
-$item-bg-color: rgb(77, 129, 234);
-
-.sb-link {
-background: var(--bg-color); //$item-bg-color;
-cursor: pointer;
-font-weight: bold;
-display: inline-block;
-color: rgb(48, 48, 48);
-
-border-radius: 1px;
-border: 1px solid black;
-> a {
- padding-block: 13px;
- padding-inline: 19px;
- display: inline-block;
- color: black;
-}
-&:hover {
- background: rgb(174, 174, 174);
- border: 1px solid rgb(134, 134, 134);
- > a {
-   color: $item-bg-color;
- }
- nuxt-link {
-   color: rgb(247, 247, 247);
- }
-}
-
-.sb-link-icon {
- width: 14px;
- height: 14px;
- color: red;
- margin-left: 9px;
- fill: purple;
- path {
-   fill: rgb(223, 40, 223);
- }
-}
-}
-</style>
+  //Temporary colors
+  $item-bg-color: rgb(77, 129, 234);
+  
+  .sb-link {
+  background: var(--bg-color); //$item-bg-color;
+  cursor: pointer;
+  font-weight: bold;
+  display: inline-block;
+  color: rgb(48, 48, 48);
+  
+  border-radius: 1px;
+  border: 1px solid black;
+  > a {
+   padding-block: 13px;
+   padding-inline: 19px;
+   display: inline-block;
+   color: black;
+  }
+  &:hover {
+   background: rgb(94, 250, 255);
+   border: 1px solid rgb(134, 134, 134);
+   > a {
+     color: $item-bg-color;
+   }
+   nuxt-link {
+     color: rgb(247, 247, 247);
+   }
+  }
+  
+  .sb-link-icon {
+   width: 14px;
+   height: 14px;
+   color: red;
+   margin-left: 9px;
+   fill: purple;
+   path {
+     fill: rgb(223, 40, 223);
+   }
+  }
+  }
+  </style>
+  
